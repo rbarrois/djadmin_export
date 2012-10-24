98a2ee328b8f6e2d1c776d95d89d8bddb9d2e2d9
Raphaël Barrois <raphael.barrois@polytechnique.org>
2012-10-25 00:09:00 +0200

    Add some tests.
    Signed-off-by: Raphaël Barrois <raphael.barrois@polytechnique.org>

--- tests/test_example.py
+++
@@ -1,6 +0,0 @@
-# coding: utf-8
-# Copyright (c) 2012 Raphaël Barrois
-# This code is distributed under the LGPLv3 License.
-
-import unittest
-
---
+++ tests/utils.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Copyright (c) 2012 Raphaël Barrois
+# This code is distributed under the LGPLv3 License.
+
+import unittest
+
+from djadmin_export import utils
+
+
+class UtilsTestCase(unittest.TestCase):
+    def test_asciify(self):
+        self.assertEqual('foo', utils.asciify('foo'))
+        self.assertEqual('bar', utils.asciify(u"bàr"))
+        self.assertEqual('Eeucna', utils.asciify(u'Ëéüçñøá'))
+
+    def test_slugify(self):
+        self.assertEqual(u'foo', utils.slugify(u"  F,ôö  "))
+        self.assertEqual(u'ba-bar', utils.slugify(u" Bà,'      bär"))
+
+
+if __name__ == '__main__':  # pragma: no cover
+    unittest.main()
